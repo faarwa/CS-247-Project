2caa5d3f68bdc9f4baf17323d528d654dabcc288
nfarwa <farwa.naqi@gmail.com>
2014-06-17 00:41:51 -0400

    Added destructor for Player

--- Player.cpp
+++ Player.cpp
@@ -2,4 +2,24 @@
 
 Player::Player(vector<Card*> cards) {
 	_cards = CardHand(cards);
+	_topCard = NULL;
+}
+
+Player::~Player() {
+	delete _topCard;
+	for (vector<Card*>::iterator it = _hearts.begin(); it != _hearts.end(); it++) {
+		delete (*it);
+	}
+
+	for (vector<Card*>::iterator it = _diamonds.begin(); it != _diamonds.end(); it++) {
+		delete (*it);
+	}
+
+	for (vector<Card*>::iterator it = _clubs.begin(); it != _clubs.end(); it++) {
+		delete (*it);
+	}
+
+	for (vector<Card*>::iterator it = _spades.begin(); it != _spades.end(); it++) {
+		delete (*it);
+	}
 }
