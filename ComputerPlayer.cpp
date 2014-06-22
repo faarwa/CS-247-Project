9004062e5d1a95ede3355bb1fdecdca507ee88d8
Jenna Tauro <jennatauro@gmail.com>
2014-06-22 12:03:29 -0400

    Fixed computer player to push onto cardsPlayed vector in proper order

--- ComputerPlayer.cpp
+++ ComputerPlayer.cpp
@@ -24,7 +24,15 @@ void ComputerPlayer::play(Card card) {
 			if (**it == **it2) {
 				deleteCard = *it;
 				cout << endl << "Player " << playerNumber() << " plays " << **it << endl;
-				cardsPlayed.at(deleteCard->getSuit()).push_back(deleteCard);
+
+				//inserts the played card into the proper spot for the static map used to keep track of all the cards played
+				if (!cardsPlayed.at(deleteCard->getSuit()).empty() && cardsPlayed.at(deleteCard->getSuit()).at(0)->getRank() > deleteCard->getRank()) {
+					//puts the played card in the front of the vector if it is less than the current lowest card played of that suit
+					cardsPlayed.at(deleteCard->getSuit()).insert(cardsPlayed.at(deleteCard->getSuit()).begin(), deleteCard);
+				} else {
+					//otherwise, puts the played card at the back of the vector
+					cardsPlayed.at(deleteCard->getSuit()).push_back(deleteCard);
+				}
 				_cards.removeCard(deleteCard);
 
 				return;
