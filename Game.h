571a2e6f4a9bace9be255d74785c9b8caa51aaf9
jetauro <jetauro@student.cs.uwaterloo.ca>
2014-07-19 22:09:31 -0400

    commented Game.h

--- Game.h
+++ Game.h
@@ -7,39 +7,37 @@
 
 using namespace std;
 
-// Game class with basic functionality for running the game (controller)
+// Game class with basic functionality for running the game (model)
 class Game : public Subject {
 public:
-	// default constructor
-	Game();								
-	void setPlayers(vector<Player*> players);
+	Game();							// default constructor								
+	void setPlayers(vector<Player*> players);		// mutator for the players of the game 
 
-	// Game progress cycle
 	void start();						// start the game
 	void newRound();					// starts a new round
-	void shuffleAndDeal();				// shuffle the deck and deal the cards to the player
+	void shuffleAndDeal();					// shuffle the deck and deal the cards to the player
 	void play();						// plays the game
 	void finishGame();					// finish the game and calculate scores
 	void ragequit();					// if a player ragequits
-	vector<Card*> getHand();										// if a player ragequits
-	vector<Player*> players() { return _players; }			// accessor - returns vector of players
-	void playOrDiscard(Card *card);
-	Player* getCurrentPlayer();
-	int currentPlayer() { return _currentPlayer; }
-	void resetCards();
-	bool isGameOver();
-	bool isRoundOver();
-	int getWinningPlayer();
-	void setSeed(int s);
-	void endCurrentGame();
+	vector<Card*> getHand();				// accessor for the players' hands
+	vector<Player*> players() { return _players; }		// accessor - returns vector of players
+	void playOrDiscard(Card *card);				// main method of the game - gets called when user clicks on a card or on a computer player's turn
+	Player* getCurrentPlayer();				// accessor for the current player
+	int currentPlayer() { return _currentPlayer; }		// accessor for the number of the current player
+	void resetCards();					// clears the cards on the board
+	bool isGameOver();					// returns true if the game is over
+	bool isRoundOver();					// returns true if the round is over
+	int getWinningPlayer();					// accessor for the number of the winning player
+	void setSeed(int s);					// mutator for the seed for shuffling
+	void endCurrentGame();					// ends the current game
 private:
-	int _seed;
-	Deck _deck;							// private data member - deck for the game
-	vector<Player*> _players;			// vector of all the players (abstract, can be human or computer)
+	int _seed;						// private data member - seed for shuffling
+	Deck _deck;						// private data member - deck for the game
+	vector<Player*> _players;				// vector of all the players (abstract, can be human or computer)
 	int _currentPlayer;					// int from 1-4 indicating which player is playing
-	bool isGameOver_;
-	bool isRoundOver_;
-	int winningPlayer_;
+	bool isGameOver_;					// boolean for if the game is over
+	bool isRoundOver_;					// boolean for if the round is over
+	int winningPlayer_;					// int from 1-4 indicating which player won
 };
 
 #endif
