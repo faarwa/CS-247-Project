4003074c73c7863c3ccde3f0cf0546d0094b79dc
jetauro <jetauro@student.cs.uwaterloo.ca>
2014-07-20 00:31:45 -0400

    commented TableCard.cpp and .h

--- TableCard.cpp
+++ TableCard.cpp
@@ -2,20 +2,23 @@
 #include "DeckGUI.h"
 #include <iostream>
 
-
+// Constructor for a table card
 TableCard::TableCard(Card *card, DeckGUI deck){
 	deck_ = deck;
 	card_ = card;
 	setTableCard(card);
 }
 
+// Destructor
 TableCard::~TableCard(){}
 
+// method to update the image on a card on the table
 void TableCard::updateFace(Card *card){
 	card_ = card;
 	setTableCard(card);
 }
 
+// sets the image of the table card based on a given card
 void TableCard::setTableCard(Card *card){
 	if(card==NULL){
 		this->set(Gdk::Pixbuf::create_from_file("img/nothing.png"));
@@ -25,6 +28,7 @@ void TableCard::setTableCard(Card *card){
 	}
 }
 
+// accessor for the card of the table card
 Card* TableCard::getCard(){
 	return card_;
 }
--- TableCard.h
+++ TableCard.h
@@ -8,16 +8,17 @@
 using std::string;
 using std::vector;
 
+// class declaration for a table card
 class TableCard : public Gtk::Image {
 public:
-	TableCard(Card *card, DeckGUI deck);
-	virtual ~TableCard();
-	void updateFace(Card *card);
-	void setTableCard(Card *card);
-	Card* getCard();
+	TableCard(Card *card, DeckGUI deck);	// Constructor for a table card
+	virtual ~TableCard();			// destructor
+	void updateFace(Card *card);		// method to update the image on a table card
+	void setTableCard(Card *card);		// mutator for the card of a table card
+	Card* getCard();			// accessor for the card of the table card
 	
 private:
-	DeckGUI deck_;
-	Card *card_;
+	DeckGUI deck_;				// private data member deck gui used to set the image
+	Card *card_;				// private data member card of the table card
 };
 #endif
